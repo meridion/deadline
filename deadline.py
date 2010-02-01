f783ac614541445c1d6d407611c086cf1160e46c
Bas Weelinck <weelinck@ow134.science.uva.nl>
2010-02-01 16:42:07 +0100

    Hai

--- deadline.py
+++ deadline.py
@@ -18,6 +18,7 @@ import select
 
 # Global settings variables
 keep_running = True
+gui_running = False
 
 # Fetch the system locale settings, so ncurses can do its job correctly
 # UTF8 strings to be precise
@@ -26,9 +27,8 @@ locale.setlocale(locale.LC_ALL, '')
 code = locale.getpreferredencoding()
 
 # Das Entrypoint
-def main(stdscr):
-	stdscr.nodelay(1)
-	gui = DeadGUI(stdscr)
+def main():
+	gui = DeadGUI()
 	while keep_running:
 		try:
 			select.select([sys.stdin], [], [])
@@ -47,139 +47,144 @@ def main(stdscr):
 
 # The deadline ncurses interface is heavily based on the irssi chat client
 class DeadGUI(object):
-	def __init__(this, stdscr):
+	def __init__(self):
+		# Autoinitialize ncurses, and make sure we clean up if we crash
+		# (or exit). This function calls main for us, with the main ncurses
+		# window as argument
+		launch_ncurses_app(self.__ncurses_init__)
+
+	def __ncurses_init__(self, stdscr):
 		# Setup input handler
-		this.special = {
-			curses.KEY_RESIZE : this.resizeEvent,
-			curses.KEY_BACKSPACE : this.promptBackspace,
-			curses.ascii.DEL : this.promptBackspace,
-			curses.KEY_LEFT : this.promptLeft,
-			curses.KEY_RIGHT : this.promptRight,
+		stdscr.nodelay(1)
+		self.special = {
+			curses.KEY_RESIZE : self.resizeEvent,
+			curses.KEY_BACKSPACE : self.promptBackspace,
+			curses.ascii.DEL : self.promptBackspace,
+			curses.KEY_LEFT : self.promptLeft,
+			curses.KEY_RIGHT : self.promptRight,
 		}
 
 		# Setup prompt
-		this.prompt = "[Main]"
-		this.string = ""
-		this.position = 0
-		this.view = 0
+		self.prompt = "[Main]"
+		self.string = ""
+		self.position = 0
+		self.view = 0
 
 		# Initialize the display
-		this.stdscr = stdscr
-		this.stdscr.clear()
-		this.height, this.width = stdscr.getmaxyx()
+		self.stdscr = stdscr
+		self.stdscr.clear()
+		self.height, self.width = stdscr.getmaxyx()
 		curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
-		this.infobarcolor = curses.A_DIM | curses.color_pair(1);
-		this.redrawFromScratch()
-		this.stdscr.refresh()
+		self.infobarcolor = curses.A_DIM | curses.color_pair(1);
+		self.redrawFromScratch()
+		self.stdscr.refresh()
 
-	def resizeEvent(this):
-		this.stdscr.clear()
-		this.height, this.width = this.stdscr.getmaxyx()
-		this.promptValidate()
-		this.redrawFromScratch()
-		this.stdscr.refresh()
+	def resizeEvent(self):
+		self.stdscr.clear()
+		self.height, self.width = self.stdscr.getmaxyx()
+		self.promptValidate()
+		self.redrawFromScratch()
+		self.stdscr.refresh()
 
-	def redrawFromScratch(this):
-		for x in range(this.width):
-			this.stdscr.addch(0, x, ' ', this.infobarcolor)
-			this.stdscr.addch(this.height - 2, x, ' ', this.infobarcolor)
+	def redrawFromScratch(self):
+		for x in range(self.width):
+			self.stdscr.addch(0, x, ' ', self.infobarcolor)
+			self.stdscr.addch(self.height - 2, x, ' ', self.infobarcolor)
 
-		this.stdscr.addstr(0, this.width / 2 - 7, "Deadline v0.1",
-			this.infobarcolor)
-		this.promptFromScratch()
+		self.stdscr.addstr(0, self.width / 2 - 7, "Deadline v0.1",
+			self.infobarcolor)
+		self.promptFromScratch()
 
-	def inputEvent(this):
-		c = this.stdscr.getch()
+	def inputEvent(self):
+		c = self.stdscr.getch()
 		if c == -1:
 			return False
 		try:
-			this.special[c]()
+			self.special[c]()
 		except KeyError:
-			this.promptInput(chr(c))
+			self.promptInput(chr(c))
 		return True
 
 	# Prompt functionality
-	def promptFromScratch(this):
-		this.stdscr.addstr(this.height - 1, 0, this.prompt)
-		this.stdscr.addstr(this.height - 1, len(this.prompt) + 1,
-			this.string[this.view:this.view + this.width -
-			len(this.prompt) - 2])
+	def promptFromScratch(self):
+		self.stdscr.addstr(self.height - 1, 0, self.prompt)
+		self.stdscr.addstr(self.height - 1, len(self.prompt) + 1,
+			self.string[self.view:self.view + self.width -
+			len(self.prompt) - 2])
 
 		# Fill with spaces if nothing is here
-		if this.position == len(this.string):
-			spacepos = len(this.prompt) + 1 + this.position - this.view
-			this.stdscr.addstr(this.height - 1, spacepos, ' ' * (this.width - 1 -
+		if self.position == len(self.string):
+			spacepos = len(self.prompt) + 1 + self.position - self.view
+			self.stdscr.addstr(self.height - 1, spacepos, ' ' * (self.width - 1 -
 				spacepos))
-		this.stdscr.move(this.height - 1, len(this.prompt) + 1 + this.position -
-			this.view)
+		self.stdscr.move(self.height - 1, len(self.prompt) + 1 + self.position -
+			self.view)
 
-	def promptInput(this, x):
-		this.string = this.string[:this.position] + x + \
-			this.string[this.position:]
-		this.position += 1;
-		if this.promptValidate():
-			this.promptFromScratch()
+	def promptInput(self, x):
+		self.string = self.string[:self.position] + x + \
+			self.string[self.position:]
+		self.position += 1;
+		if self.promptValidate():
+			self.promptFromScratch()
 		else:
 			# Put the character before the cursor, and the cursor in the new
 			# current position.
-			this.stdscr.insch(this.height - 1, len(this.prompt) +
-				this.position - this.view, x)
-			this.stdscr.move(this.height - 1, len(this.prompt) + 1 +
-				this.position - this.view)
+			self.stdscr.insch(self.height - 1, len(self.prompt) +
+				self.position - self.view, x)
+			self.stdscr.move(self.height - 1, len(self.prompt) + 1 +
+				self.position - self.view)
 
-	def promptBackspace(this):
-		if this.position != 0:
-			this.string = this.string[:this.position - 1] + \
-				this.string[this.position:]
-			this.position -= 1
-			if this.promptValidate():
-				this.promptFromScratch()
+	def promptBackspace(self):
+		if self.position != 0:
+			self.string = self.string[:self.position - 1] + \
+				self.string[self.position:]
+			self.position -= 1
+			if self.promptValidate():
+				self.promptFromScratch()
 			else:
-				this.stdscr.delch(this.height - 1, len(this.prompt) +
-					1 + this.position - this.view)
-				this.stdscr.move(this.height - 1, len(this.prompt) +
-					1 + this.position - this.view)
+				self.stdscr.delch(self.height - 1, len(self.prompt) +
+					1 + self.position - self.view)
+				self.stdscr.move(self.height - 1, len(self.prompt) +
+					1 + self.position - self.view)
 
-	def promptLeft(this):
-		if this.position != 0:
-			this.position -= 1
-			if this.promptValidate():
-				this.promptFromScratch()
+	def promptLeft(self):
+		if self.position != 0:
+			self.position -= 1
+			if self.promptValidate():
+				self.promptFromScratch()
 			else:
-				this.stdscr.move(this.height - 1, len(this.prompt) + 1 +
-					this.position - this.view)
+				self.stdscr.move(self.height - 1, len(self.prompt) + 1 +
+					self.position - self.view)
 
-	def promptRight(this):
-		if this.position != len(this.string):
-			this.position += 1
-			if this.promptValidate():
-				this.promptFromScratch()
+	def promptRight(self):
+		if self.position != len(self.string):
+			self.position += 1
+			if self.promptValidate():
+				self.promptFromScratch()
 			else:
-				this.stdscr.move(this.height - 1, len(this.prompt) + 1 +
-					this.position - this.view)
+				self.stdscr.move(self.height - 1, len(self.prompt) + 1 +
+					self.position - self.view)
 
 	# Verify that the prompt is in a displayable state
 	# If it is not, fix it and return True, otherwise return False
-	def promptValidate(this):
+	def promptValidate(self):
 		# If we scroll too much to left (or backspace)
 		# We need the terminal to scroll the text
 		# View defines how much our text is scrolled
-		if this.position - this.view < 0:
-			this.view = this.view - this.width / 4
-			if this.view < 0:
-				this.view = 0
+		if self.position - self.view < 0:
+			self.view = self.view - self.width / 4
+			if self.view < 0:
+				self.view = 0
 			return True
 
 		# Same but now for to the right
-		if this.position - this.view > this.width - 2 - len(this.prompt):
-			this.view = this.view + this.width / 4
-			if this.view > this.position:
-				this.view = this.position
+		if self.position - self.view > self.width - 2 - len(self.prompt):
+			self.view = self.view + self.width / 4
+			if self.view > self.position:
+				self.view = self.position
 			return True
 		return False
 
-# Autoinitialize ncurses, and make sure we clean up if we crash (or exit)
-# This function calls main for us, with the main ncurses window as argument
-launch_ncurses_app(main)
+main()
 print ""
 
